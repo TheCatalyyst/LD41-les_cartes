7edea10429442cfe31850e0d7db24c0d1d443a4b
Corentin Le Lez <corentin.lelez@gmail.com>
2018-04-21 15:16:13 +0200

    cartes sheild/attack

--- cartes/carte_base.gd
+++ cartes/carte_base.gd
@@ -3,24 +3,24 @@ extends RigidBody2D
 export(float) var speed;
 export(float) var rot;
 
+export (int) var player;
+
+export (int) var dmgToPlayer;
+
+var selected
 func _ready():
+	selected=false;
 	set_mode(MODE_STATIC)
-	lancer(Vector2 (1,0))
 
 
 func retourner_carte():
 	if($visible.visible):
 		$visible.hide()
+		selected = false;
 	else:
 		$visible.show()
+		selected = true;
 
-func _on_Area2D_body_entered(body):
-	# Une carte est rentrée
-	# Faut faire l'action associé
-	
-	# à faire en héritage dans le modèle de carte précis ?
-	
-	pass
 func lancer(dir):
 	set_mode(MODE_RIGID)
 	set_linear_velocity(dir.normalized()*speed)
