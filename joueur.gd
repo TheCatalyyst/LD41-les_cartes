689501e52d412e7898aeca621b1992780afac156
Rémi MONTHILLER <remi.monthiller@etu.enseeiht.fr>
2018-04-21 15:14:09 +0200

    pioche de carte

--- joueur.gd
+++ joueur.gd
@@ -1,9 +1,34 @@
-extends Area2D
+extends Node2D
 
+export(int) var nb_cartes
+export(int) var carte_select
 export(float) var vie
-export(int) var num
+export(Array) var pioche
 
+func _ready():
+	piocher_carte()
+	piocher_carte()
+	piocher_carte()
+	piocher_carte()
+	piocher_carte()
 
-func _on_joueur_body_entered(body):
+func _process(delta):
+	if(Input.is_action_just_pressed("ui_up")):
+		carte_select = (carte_select + 1) % nb_cartes
+		$Cartes.get_child(carte_select).retourner_carte()
+	if(Input.is_action_just_pressed("ui_down")):
+		carte_select = (carte_select - 1) % nb_cartes
+		$Cartes.get_child(carte_select).retourner_carte()
+
+func _on_Area2D_body_entered(body):
 	vie -= 1
-	body.queu_free()
+	
+func piocher_carte():
+	var child_id = 0
+	while(child_id < $Cartes.get_child_count() && !$Cartes.get_child(child_id).est_vide()):
+		child_id += 1
+		
+	if(child_id < $Cartes.get_child_count()):
+		var carte_pioche = load(pioche[randi() % pioche.size()]).instance()
+		$Cartes.get_child(child_id).add_child(carte_pioche)
+	
---
+++ joueurs/conteneur_carte.gd
@@ -0,0 +1,4 @@
+extends Position2D
+
+func est_vide():
+	return (get_child_count() == 0)
